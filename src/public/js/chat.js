133163993f4e72033817efaf976be7f88cbf5cab
Rasmus Povlsen <git@rpovlsen.com>
2021-05-30 13:47:18 +0200

    Fix chat bug

--- src/public/js/chat.js
+++ src/public/js/chat.js
@@ -1,7 +1,6 @@
-const url = window.location.hostname;
-
-const socket = io(`${url}:8080`, { autoConnect: false });
-
 window.addEventListener('load', () => {
+  const url = window.location.hostname;
+
+  const socket = io(`${url}:8080`, { autoConnect: false });
   socket.connect();
 });
