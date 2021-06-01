cc087705df11cdfbb1f42d34df8750ed16653011
Rasmus Povlsen <git@rpovlsen.com>
2021-06-01 14:12:15 +0200

    chat merev

--- src/chat.js
+++ src/chat.js
@@ -1,9 +1,3 @@
-const io = require('socket.io')({
-  cors: {
-    origin: true,
-  },
-});
-
 const messages = [{
   username: 'TheCoolChatMan',
   message: 'Wow, this is a really cool chat!',
@@ -13,24 +7,26 @@ const messages = [{
 },
 ];
 
-io.on('connection', (socket) => {
-  console.log(`Connected: ${socket.id}`);
-  messages.forEach((message) => {
-    socket.emit('CHAT_MESSAGE', message);
-  });
-
-  socket.on('CHAT_MESSAGE', (message) => {
-    // TODO: Verify user identity. Currently we blindly trust the user is who they say they are.
-    // TODO: Validate message.
-    messages.push(message);
-    if (messages.length > 100) {
-      messages.splice(0, 1);
-    }
-    socket.broadcast.emit('CHAT_MESSAGE', message);
-  });
-});
-
-io.listen(8080);
 
 // Export empty function so this file can be imported and executed.
-module.exports = () => {};
+module.exports = (serv) => {
+  // eslint-disable-next-line global-require
+  const io = require('socket.io')(serv);
+
+  io.on('connection', (socket) => {
+    console.log(`Connected: ${socket.id}`);
+    messages.forEach((message) => {
+      socket.emit('CHAT_MESSAGE', message);
+    });
+
+    socket.on('CHAT_MESSAGE', (message) => {
+      // TODO: Verify user identity. Currently we blindly trust the user is who they say they are.
+      // TODO: Validate message.
+      messages.push(message);
+      if (messages.length > 100) {
+        messages.splice(0, 1);
+      }
+      socket.broadcast.emit('CHAT_MESSAGE', message);
+    });
+  });
+};
