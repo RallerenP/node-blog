de0d4424bc77346d8254e1d8221a56079664af44
Rasmus Povlsen <git@rpovlsen.com>
2021-06-12 14:51:14 +0200

    Proper close #4

---
+++ src/public/js/fragments/head.js
@@ -0,0 +1,20 @@
+async function setup() {
+  const loggedInResponse = await fetch('/api/auth/me', {
+    method: 'GET',
+  });
+
+  const loggedIn = await loggedInResponse.json();
+
+  console.log(loggedIn.email);
+
+  if (loggedIn.email) {
+    const selectButtonGroup = $('#auth-buttons');
+    selectButtonGroup[0].children[0].style.display = 'none';
+    selectButtonGroup[0].children[1].style.display = 'none';
+    selectButtonGroup.append(`
+        <a href="/api/auth/logout"><button class="btn btn-danger mr-2">Logout</button></a>
+        <button class="btn btn-secondary mr-2" type="button"> Logged in as ${loggedIn.email} </button>
+      `);
+  }
+}
+setup();
